a1a3cc4ea2e71d9fa4620aa3ee533bec4fd2d4c9
Parikshith Palegar <parikshith@Parikshiths-MacBook-Air.local>
2022-02-21 20:47:32 +0530

    added translator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import FlexItem from './FlexItem'
+import codes from './codes'
 
-export class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>Morse Coder</h1>
 
-        <div className='flex-box'>
-          <FlexItem h3="massage" placer="Enter the massage here......." />
-          <FlexItem h3="code" placer="Copy code......." />
-          <button className='copy-btn normal'>Copy</button>
-        </div></div>
-    )
+export default function App() {
+
+
+  var [massage, setMassage] = useState("")
+  var [code, setCode] = useState("")
+  var word_chars = massage.split("")
+
+  function traslate() {
+
+    var morseCode = ""
+    for (let ch in word_chars) {
+      morseCode += codes[ch] + " "
+    }
+    setCode(morseCode)
   }
-}
 
-export default App
+
+  return (
+    <div className='app'>
+      <h1>Morse Coder</h1>
+
+      <div className='flex-box'>
+        <FlexItem key="1" h3="massage" translate={traslate} set={setMassage} val={massage} placehold="Enter the massage here......." />
+        <FlexItem key="2" h3="code" id="code-box" translate={traslate} val={code} set={setCode} placehold="Copy code........" />
+
+        <button className='copy-btn normal' >Copy</button>
+      </div></div>
+  )
+}
--- src/FlexItem.jsx
+++ src/FlexItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default function FlexItem(props) {
+
+
     return (
         <div className='flex-box-item'>
             <h3>{props.h3}</h3>
-            <textarea type="text" name="" cols="100" rows="12" placeholder={props.placer} id="" />
+            <textarea type="text" onChange={(e) => {
+                props.set(e.target.value)
+                props.translate()
+            }} className={props.id} name="" placeholder={props.placehold} cols="100" rows="12" value={props.val} id="" />
         </div>
     )
 }
